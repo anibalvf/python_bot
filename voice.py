c767aa061c0c0bb58cbe240d8652a510b66d21aa
anibalvf <33007157+anibalvf@users.noreply.github.com>
2020-12-23 21:14:06 +0100

    last version

--- voice.py
+++ voice.py
@@ -3,22 +3,21 @@ import discord
 from discord.ext import commands
 import youtube_dl
 import os
+import random
+import time
+from discord import FFmpegPCMAudio
+from discord.utils import get
 
 
 client = commands.Bot(command_prefix="!")
 
+# # Setting `Playing ` status # await bot.change_presence(activity=discord.Game(name="a game"))
 
-# # Setting `Playing ` status
-# await bot.change_presence(activity=discord.Game(name="a game"))
+# # Setting `Streaming ` status # await bot.change_presence(activity=discord.Streaming(name="My Stream", url=my_twitch_url))
 
-# # Setting `Streaming ` status
-# await bot.change_presence(activity=discord.Streaming(name="My Stream", url=my_twitch_url))
+# # Setting `Listening ` status # await bot.change_presence(activity=discord.Activity(type=discord.ActivityType.listening, name="a song"))
 
-# # Setting `Listening ` status
-# await bot.change_presence(activity=discord.Activity(type=discord.ActivityType.listening, name="a song"))
-
-# # Setting `Watching ` status
-# await bot.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name="a movie"))
+# # Setting `Watching ` status # await bot.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name="a movie"))
 
 @client.event
 async def on_ready():
@@ -32,29 +31,89 @@ async def on_ready():
 async def que(ctx):
     await ctx.send("QUEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEE")
 
+
+
+@client.command()
+async def shisha(ctx):
+    eleccion = ["YEAAAH","YEAAAH"]
+    await ctx.send("Decidiendo shisha rejuvenecedora")
+    time.sleep(1)
+    await ctx.send("3")
+    time.sleep(1)
+    await ctx.send("2")
+    time.sleep(1)
+    await ctx.send("1")
+    time.sleep(1)
+
+    resultado = random.choice(eleccion)
+    
+    embed = discord.Embed(title="HAY SHISHA?",description=resultado,color=0xff0000)
+    embed.set_author(name="Anibal")
+    await ctx.send(embed=embed)
+    if resultado =="YEAAAH":
+        if ctx.author.voice==None:
+            await ctx.send("No puedes escuchar la respuesta conectate a un canal devoz")
+        else:
+            try:
+                channel = ctx.author.voice.channel
+                await channel.connect()
+                voice = discord.utils.get(client.voice_clients, guild=ctx.guild)
+            except:
+                voice = discord.utils.get(client.voice_clients, guild=ctx.guild)
+            voice.play(discord.FFmpegPCMAudio("audios/guycoughing.mp3"))  
+    
+
+
+@client.command()
+async def dominos(ctx):
+    eleccion = ["YEAAAH","JAMAS"]
+
+    await ctx.send("Decidiendo dominos en la maquina que da vueltas")
+    time.sleep(1)
+    await ctx.send("3")
+    time.sleep(1)
+    await ctx.send("2")
+    time.sleep(1)
+    await ctx.send("1")
+    time.sleep(1)
+
+    resultado = random.choice(eleccion)
+    
+    embed = discord.Embed(title="DOMINOS?",description=resultado,color=0xff0000)
+    embed.set_author(name="Anibal")
+    await ctx.send(embed=embed)
+
+    if resultado =="JAMAS":
+        if ctx.author.voice==None:
+            await ctx.send("No puedes escuchar la respuesta conectate a un canal devoz")
+        else:
+            try:
+                channel = ctx.author.voice.channel
+                await channel.connect()
+                voice = discord.utils.get(client.voice_clients, guild=ctx.guild)
+            except:
+                voice = discord.utils.get(client.voice_clients, guild=ctx.guild)
+            voice.play(discord.FFmpegPCMAudio("audios/guycoughing.mp3"))    
+
+
+
+@client.command()
+async def insultame(ctx):
+    insultos = ["Gandúl","Comeflores","Capullo","Cernícalo","Comebolsas","Huevón","Hijo de puta","Inutil","Homo","Dario el cenizo"]
+
+    # down()
+    await ctx.message.author.send("Eres un "+random.choice(insultos))
+
 @client.command()
 async def borrar(ctx):
     partes = ctx.message.content.split()
     if int(partes[1])<=10:
         await ctx.channel.purge(limit=int(partes[1]))
     else:
-        await ctx.send("no puedes borrar mas de 10 mensajes a la vez")   
+        await ctx.send("no puedes borrar mas de 10 mensajes a la vez")
 
-
-@client.command()
-async def play(ctx, url : str):
-    song_there = os.path.isfile("song.mp3")
-    try:
-        if song_there:
-            os.remove("song.mp3")
-    except PermissionError:
-        await ctx.send("me cago en mi puta madre ERROR")
-        return
-
-    voiceChannel = discord.utils.get(ctx.guild.voice_channels, name='GRUPO1')
-    await voiceChannel.connect()
-    voice = discord.utils.get(client.voice_clients, guild=ctx.guild)
-
+def down():
+    url="https://www.youtube.com/watch?v=rS9VRg-TOgM"
     ydl_opts = {
         'format': 'bestaudio/best',
         'postprocessors': [{
@@ -67,8 +126,57 @@ async def play(ctx, url : str):
         ydl.download([url])
     for file in os.listdir("./"):
         if file.endswith(".mp3"):
-            os.rename(file, "song.mp3")
-    voice.play(discord.FFmpegPCMAudio("song.mp3"))
+            os.rename(file, "guycoughing.mp3")    
+
+@client.command(pass_context=True)
+async def test(ctx):
+
+    if ctx.author.voice==None:
+        await ctx.send("Jamas no estas conectado a un canal de voz")
+    else:
+        try:
+            channel = ctx.author.voice.channel
+            await channel.connect()
+            voice = discord.utils.get(client.voice_clients, guild=ctx.guild)
+        except discord.ClientException:
+            await client.say('ya estoy en un canal')    
+
+@client.command()
+async def play(ctx, url : str):
+    song_there = os.path.isfile("song.mp3")
+    try:
+        if song_there:
+            os.remove("song.mp3")
+    except PermissionError:
+        print("error")
+        return
+    try:
+        if ctx.author.voice==None:
+            await ctx.send("Jamas no estas conectado a un canal de voz")
+        else:
+            try:
+                channel = ctx.author.voice.channel
+                await channel.connect()
+                voice = discord.utils.get(client.voice_clients, guild=ctx.guild)
+            except discord.ClientException:
+                voice = discord.utils.get(client.voice_clients, guild=ctx.guild)
+            ydl_opts = {
+                'format': 'bestaudio/best',
+                'postprocessors': [{
+                    'key': 'FFmpegExtractAudio',
+                    'preferredcodec': 'mp3',
+                    'preferredquality': '192',
+                }],
+            }
+            with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+                ydl.download([url])
+            for file in os.listdir("./"):
+                if file.endswith(".mp3"):
+                    os.rename(file, "song.mp3")
+            voice.play(discord.FFmpegPCMAudio("song.mp3"))
+    except:
+        print("ERROR! ME CAGO EN LA PUTA")
+
 
 
 @client.command()
